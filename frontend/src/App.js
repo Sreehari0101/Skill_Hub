51651019cb8892e031867184544cfea7ebb86fd7
sreehari0101 <sreehariprolos@gmail.com>
2023-11-25 15:23:38 +0530

    Configured Tailwind CSS and NextUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Sidebar from './Components/Sidebar';
+import './index.css';
+import {NextUIProvider} from "@nextui-org/react";
 
 function App() {
   return (
+    <NextUIProvider>
     <div className="App">
-    <Sidebar/>
+     <h1 className="text-5xl">
+      Hello world!
+    </h1>
     </div>
+    </NextUIProvider>
   ); 
 }
 
