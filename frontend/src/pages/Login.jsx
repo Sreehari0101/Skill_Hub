212f6fcba576f48a642c0a622a9804dc1d344be7
sreehari0101 <sreehariprolos@gmail.com>
2024-01-19 21:35:41 +0530

    Register  Page  completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./css/Login.css";
 import { Input } from "@nextui-org/react";
+import {EyeFilledIcon} from "../Components/EyeFilledIcon";
+import {EyeSlashFilledIcon} from "../Components/EyeSlashFilledIcon";
 import loginImage from "../assets/Login_Image.png";
 import Logo from "./../assets/SkillHub_Sidebar_logo.png";
+import { Link } from 'react-router-dom'
 
 function Login() {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
   return (
     <div className="login-page">
       <div className="top">
@@ -15,9 +21,11 @@ function Login() {
           </div>
         </div>
         <div className="register-direct">
+        <Link to="/register">
           <button>
             <div className="register-button">Register</div>
           </button>
+        </Link>
         </div>
       </div>
       <div className="bottom">
@@ -27,7 +35,20 @@ function Login() {
               <h1>Login to your account</h1>
               <div className="flex flex-col w-full gap-5">
                 <Input type="email" label="Enter your Email" />
-                <Input type="email" label="Password" />
+                <Input
+      label="Password"
+      variant="flat"
+      endContent={
+        <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+          {isVisible ? (
+            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          ) : (
+            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          )}
+        </button>
+      }
+      type={isVisible ? "text" : "password"}
+    />
               </div>
               <div className="login-button-container">
                 <div className="login-button">
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import "./css/Register.css";
+import { Input } from "@nextui-org/react";
+import {EyeFilledIcon} from "../Components/EyeFilledIcon";
+import {EyeSlashFilledIcon} from "../Components/EyeSlashFilledIcon";
+import registerImage from "../assets/Register_Image.png";
+import Logo from "./../assets/SkillHub_Sidebar_logo.png";
 
 function Register() {
+  const [isVisible, setIsVisible] = React.useState(false);
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
   return (
-    <div>Register</div>
-  )
+    <div className="register-page">
+      <div className="top">
+        <div className="logo-container">
+          <div className="logo-section">
+            <img src={Logo} className="logo-image" alt="SkillHub-logo" />
+            <div class="logo-description">EMPOWERING FUTURES</div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="register-container">
+          <div className="register-content">
+            <div className="register-form">
+              <h1>Create your account</h1>
+              <div className="flex flex-col w-full gap-5">
+                <Input type="email" label="Enter your Email" />
+                <Input
+      label="Password"
+      variant="flat"
+      endContent={
+        <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+          {isVisible ? (
+            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          ) : (
+            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          )}
+        </button>
+      }
+      type={isVisible ? "text" : "password"}
+    />
+
+<Input
+      label=" Confirm Password"
+      variant="flat"
+      endContent={
+        <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+          {isVisible ? (
+            <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          ) : (
+            <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+          )}
+        </button>
+      }
+      type={isVisible ? "text" : "password"}
+    />
+              </div>
+              <div className="register-button-container">
+                <div className="register-button">
+                  <button>Register</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="register-image-container">
+            <img className="register-image" src={registerImage} alt="register" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
