615eb96fe3ee3d4e7455bf7af841aea7a30b5975
sreehari0101 <sreehariprolos@gmail.com>
2024-01-19 20:04:05 +0530

    Login Page  completed

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./css/Login.css";
 import { Input } from "@nextui-org/react";
+import loginImage from "../assets/Login_Image.png";
 import Logo from "./../assets/SkillHub_Sidebar_logo.png";
 
 function Login() {
@@ -23,14 +24,21 @@ function Login() {
         <div className="login-container">
           <div className="login-content">
             <div className="login-form">
-            <h1>Login to your account</h1>
-            <div className="flex flex-col w-9/12 gap-5">
-              <Input type="email" label="Enter your Email" />
-              <Input type="email" label="Password" />
-            </div>
+              <h1>Login to your account</h1>
+              <div className="flex flex-col w-full gap-5">
+                <Input type="email" label="Enter your Email" />
+                <Input type="email" label="Password" />
+              </div>
+              <div className="login-button-container">
+                <div className="login-button">
+                  <button>Login</button>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="login-image-container"></div>
+          <div className="login-image-container">
+            <img className="login-image" src={loginImage} alt="login" />
+          </div>
         </div>
       </div>
     </div>
