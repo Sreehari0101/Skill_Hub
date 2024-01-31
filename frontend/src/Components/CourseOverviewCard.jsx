9289c94b3b92b4f31705b2e132936302cf5e3f9c
sreehari0101 <sreehariprolos@gmail.com>
2024-01-31 13:31:53 +0530

    courseOverview card component

---
+++ frontend/src/Components/CourseOverviewCard.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./css/CourseOverviewCard.css";
+import Video_Lecture_Image from "./../assets/Video_Lecture_Image.png"
+import Notes_Image from "./../assets/Notes_Image.png"
+import Certificate_Image from "./../assets/Certificate_Image.png"
+
+
+function CourseOverviewCard() {
+  return (
+    <div className='Course-overview'>
+        <h1>What’s Included</h1>
+    <div className="Course-overview-contents">
+        <div className="video-lecture-image-container">
+        <img className="rating-icon" src={Video_Lecture_Image} alt="Icon" />
+        <h2>Video Lectures</h2>
+        </div>
+        <div className="notes-image-container">
+        <img className="rating-icon" src={Notes_Image} alt="Icon" />
+        <h2>Notes</h2>
+        </div>
+        <div className="certificate-image-container">
+        <img className="rating-icon" src={Certificate_Image} alt="Icon" />
+        <h2>Certificate of Completion</h2>
+        </div>
+      </div>
+      </div>
+  )
+}
+
+export default CourseOverviewCard
--- frontend/src/pages/Student/CourseDetails.jsx
+++ frontend/src/pages/Student/CourseDetails.jsx
@@ -5,6 +5,7 @@ import ProfileCard from "../../Components/ProfileCard";
 import "./css/CourseDetails.css";
 import profile_icon from "../../assets/Profile_icon.jpg";
 import Cover1 from "../../assets/Data_Analytics_Course_icon.jpg";
+import CourseOverviewCard from "../../Components/CourseOverviewCard";
 
 function CourseDetails() {
   return (
@@ -27,6 +28,7 @@ function CourseDetails() {
           courseMembers="10K"
           courseBio="The Data Analyst Course offers a comprehensive bootcamp, equipping learners with skills in data analysis. Covering tools, techniques, and real-world applications for a successful career in data analysis."
         />
+      <CourseOverviewCard />
       </div>
     </div>
   );
