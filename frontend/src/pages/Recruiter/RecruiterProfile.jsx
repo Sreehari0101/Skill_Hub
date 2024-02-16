5dcaf4cdb6388c33469d2f9a8c6a1d2608375cee
sreehari0101 <sreehariprolos@gmail.com>
2024-02-16 15:37:48 +0530

    update logo functionality in company profile

--- frontend/src/pages/Recruiter/RecruiterProfile.jsx
+++ frontend/src/pages/Recruiter/RecruiterProfile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./css/RecruiterProfile.css";
 import { Input } from "@nextui-org/react";
 import { Textarea } from "@nextui-org/react";
@@ -8,17 +8,51 @@ import { CameraIcon } from "../../Components/CameraIcon";
 import companyIcon from "../../assets/Netflix_logo.jpg";
 
 function RecruiterProfile() {
+  const [logoPreview, setLogoPreview] = useState(companyIcon);
+
+  const fileInputRef = useRef(null);
+
+  const handleLogoChange = (event) => {
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setLogoPreview(reader.result);
+      };
+      reader.readAsDataURL(selectedFile);
+    }
+  };
+
+  const handleUpdateLogoClick = () => {
+    fileInputRef.current.click();
+  };
+
   return (
-    <div className="recruiter-profile ">
+    <div className="recruiter-profile">
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleLogoChange}
+        ref={fileInputRef}
+        style={{ display: "none" }}
+      />
+
       <Avatar
-        src={companyIcon}
+        src={logoPreview}
         size="sm"
         className="w-40 h-40 text-large mx-auto mb-3"
       />
-      <Button className=" bg-black text-white" endContent={<CameraIcon />}>
+
+      <Button
+        className="bg-black text-white"
+        endContent={<CameraIcon />}
+        onClick={handleUpdateLogoClick}
+      >
         Update Logo
       </Button>
-      <div className=" w-5/6 mb-8 mt-8 ">
+
+      <div className="w-5/6 mb-8 mt-8">
         <Input type="text" label="Company Name" className="mb-5" />
         <Textarea
           label="Company Description"
@@ -28,7 +62,8 @@ function RecruiterProfile() {
         />
         <Input type="text" label="Company Website" className="mb-5" />
         <Input type="text" label="Company Email" className="mb-5" />
-        <div className="flex justify-end ">
+
+        <div className="flex justify-end">
           <Button className="py-5 px-10 rounded-lg bg-black text-white">
             Save
           </Button>
